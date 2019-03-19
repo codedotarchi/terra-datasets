a8f8c8c86e356af7832c77d5cfd6a71ed646eeab
Noah Pitts <noahpitts@code.archi>
2019-03-18 22:41:49 -0700

    building first run

--- scripts/build_dataset.py
+++ scripts/build_dataset.py
@@ -5,16 +5,16 @@ from osgeo import gdal
 from PIL import Image, ImageFilter
 
 
-directory = '../temp'
+directory = '../public'
 
 location = 'Yos'
 
-patch_sizes = [2048, 1024, 512, 256, 128, 64]
+patch_sizes = [512, 256]
 
 num_samples = {
-    'train': 10,
-    'test': 2,
-    'val': 3
+    'train': 400,
+    'test': 20,
+    'val': 20
 }
 
 def read_raw_channel_as_array(directory, location, channel_name):
@@ -42,9 +42,9 @@ raw_channel_data = {
 }
 
 # dups for generated data
-raw_channel_data['aspectbin8'] = raw_channel_data['aspect']
-raw_channel_data['hillshadebin4'] = raw_channel_data['hillshade']
-raw_channel_data['slopebin5'] = raw_channel_data['slope']
+# raw_channel_data['aspectbin8'] = raw_channel_data['aspect']
+# raw_channel_data['hillshadebin4'] = raw_channel_data['hillshade']
+# raw_channel_data['slopebin5'] = raw_channel_data['slope']
 
 raw_channel_data['grid8bin2'] = raw_channel_data['topo']
 raw_channel_data['grid8bin4'] = raw_channel_data['topo']
@@ -66,14 +66,14 @@ def scale_array(X, x_min, x_max):
 
 # Bins the Z value into a certain number of bins
 def bin_array(X, num_bins):
-    return scale_array(X, 0, num_bins).astype(np.uint8)
+    return scale_array(X, 0, num_bins)
 
 # Set Val in np array to new val
 def set_data_val(X, val, new_val):
     for i in range(0, X.shape[0]):
         for j in range(0, X.shape[1]):
-            if X.shape[i][j] == val:
-                X.shape[i][j] = new_val
+            if X[i][j] == val:
+                X[i][j] = new_val
 
 # Set Vals based on divisions
 def bin_array_divs(X, divs):
@@ -81,88 +81,136 @@ def bin_array_divs(X, divs):
         for j in range(0, X.shape[1]):
             val = 0
             for d in range(len(divs)):
-                if X.shape[i][j] >= divs[d]:
+                if X[i][j] >= divs[d]:
                     val += 1
-            X.shape[i][j] = val
+            X[i][j] = val
 
 # channel specific processing procedures...should return A PIL image (8-bit) between 0 - 255 vals
 def process_channel(data_arr, channel, patch_size):
     if channel == 'grid8bin2':
-        patch = bin_array(data_arr, 2)
-        patch = scale_array(data_arr, 0, 256)
+        patch = data_arr.copy()
+        patch = scale_array(patch, 0, 256)
         patch = patch.astype(np.uint8)
         image = Image.fromarray(patch, mode='L')
         image = image.resize((8, 8), resample=Image.HAMMING)
-        return image.resize((patch_size, patch_size), resample=Image.BOX)
+
+        # binary thresehold
+        for i in range(0, 8):
+            for j in range(0, 8):
+                val = image.getpixel((i, j))
+                if val < 128: val = 0
+                else: val = 255
+                image.putpixel((i, j), val)
+
+        image = image.resize((patch_size, patch_size), resample=Image.BOX)
+        return image
     
     elif channel == 'grid8bin4':
-        patch = bin_array(data_arr, 4)
-        patch = scale_array(data_arr, 0, 256)
+        patch = data_arr.copy()
+        patch = scale_array(patch, 0, 256)
         patch = patch.astype(np.uint8)
         image = Image.fromarray(patch, mode='L')
         image = image.resize((8, 8), resample=Image.HAMMING)
-        return image.resize((patch_size, patch_size), resample=Image.BOX)
+
+        # binary thresehold
+        for i in range(0, 8):
+            for j in range(0, 8):
+                val = image.getpixel((i, j))
+                if val < 64: val = 0
+                elif val >= 64 and val < 128: val = 85
+                elif val >= 128 and val < 192: val = 170
+                else: val = 255
+                image.putpixel((i, j), val)
+
+        image = image.resize((patch_size, patch_size), resample=Image.BOX)
+        return image
 
     elif channel == 'grid16bin2':
-        patch = bin_array(data_arr, 2)
-        patch = scale_array(data_arr, 0, 256)
+        patch = data_arr.copy()
+        patch = scale_array(patch, 0, 256)
         patch = patch.astype(np.uint8)
         image = Image.fromarray(patch, mode='L')
         image = image.resize((16, 16), resample=Image.HAMMING)
-        return image.resize((patch_size, patch_size), resample=Image.BOX)
+
+        # binary thresehold
+        for i in range(0, 16):
+            for j in range(0, 16):
+                val = image.getpixel((i, j))
+                if val < 128: val = 0
+                else: val = 255
+                image.putpixel((i, j), val)
+
+
+        image = image.resize((patch_size, patch_size), resample=Image.BOX)
+        return image
     
     elif channel == 'grid16bin4':
-        patch = bin_array(data_arr, 4)
-        patch = scale_array(data_arr, 0, 256)
+        patch = data_arr.copy()
+        patch = scale_array(patch, 0, 256)
         patch = patch.astype(np.uint8)
         image = Image.fromarray(patch, mode='L')
         image = image.resize((16, 16), resample=Image.HAMMING)
-        return image.resize((patch_size, patch_size), resample=Image.BOX)
 
-    elif channel == 'aspectbin8':
-        d_min = data_arr.min()
-        if d_min < -3.4e30:
-            set_data_val(data_arr, d_min, 0.0)
+        # binary thresehold
+        for i in range(0, 16):
+            for j in range(0, 16):
+                val = image.getpixel((i, j))
+                if val < 64: val = 0
+                elif val >= 64 and val < 128: val = 85
+                elif val >= 128 and val < 192: val = 170
+                else: val = 255
+                image.putpixel((i, j), val)
 
-        patch = bin_array(data_arr, 8)
-        patch = scale_array(data_arr, 0, 256)
-        patch = patch.astype(np.uint8)
-        image = Image.fromarray(patch, mode='L')
-        return image.resize((patch_size, patch_size), resample=Image.NEAREST)
-
-    elif channel == 'slopebin5':
-        d_min = data_arr.min()
-        if d_min < -3.4e30:
-            set_data_val(data_arr, d_min, 0.0)
-
-        # bins 2>=d | 2<d<=8 | 8<d<=32 | 32<d<=64 | 64<d
-        bin_array_divs(data_arr, [2, 8, 32, 64])
-
-        patch = scale_array(data_arr, 0, 256)
-        patch = patch.astype(np.uint8)
-        image = Image.fromarray(patch, mode='L')
-        return image.resize((patch_size, patch_size), resample=Image.NEAREST)
-
-    elif channel == 'hillshadebin4':
-        patch = bin_array(data_arr, 4)
-        patch = scale_array(data_arr, 0, 256)
-        patch = patch.astype(np.uint8)
-        image = Image.fromarray(patch, mode='L')
-        return image.resize((patch_size, patch_size), resample=Image.NEAREST)
+        image = image.resize((patch_size, patch_size), resample=Image.BOX)
+        return image
 
     elif channel == 'hydro':
-        patch = scale_array(data_arr, 0, 256)
+        patch = data_arr.copy()
+        for i in range(0, patch.shape[0]):
+            for j in range(0, patch.shape[1]):
+                if patch[i][j] == 1: patch[i][j] = 255
+
         patch = patch.astype(np.uint8)
         image = Image.fromarray(patch, mode='L')
-        image = image.filter(ImageFilter.GaussianBlur(radius=3))
-        return image.resize((patch_size, patch_size), resample=Image.HAMMING)
+        image = image.resize((patch_size, patch_size), resample=Image.HAMMING)
+        # image = image.filter(ImageFilter.GaussianBlur(radius=int(patch_size/100)))
+        return image
     # Default Process for general 8-bit conversion
-    else:  
-        d_min = data_arr.min()
-        if d_min < -3.4e30:
-            set_data_val(data_arr, d_min, 0.0)
 
-        patch = scale_array(data_arr, 0, 256)
+    elif channel == 'contour100':
+        patch = data_arr.copy()
+        for i in range(0, patch.shape[0]):
+            for j in range(0, patch.shape[1]):
+                if patch[i][j] > 65000 or patch[i][j] == 0: patch[i][j] = 0
+                else: patch[i][j] = 255
+
+        patch = patch.astype(np.uint8)
+        image = Image.fromarray(patch, mode='L')
+        image = image.resize((patch_size, patch_size), resample=Image.HAMMING)
+        # image = image.filter(ImageFilter.GaussianBlur(radius=int(patch_size/100)))
+        return image
+
+    elif channel == 'slope':
+        patch = data_arr.copy()
+        d_min = patch.min()
+        if d_min < -3.4e30:
+            set_data_val(patch, d_min, 0.0)
+
+        patch = scale_array(patch, 0, 256)
+        patch = patch.astype(np.uint8)
+        image = Image.fromarray(patch, mode='L')
+        image = image.resize((patch_size, patch_size), resample=Image.HAMMING)
+        image = image.filter(ImageFilter.GaussianBlur(radius=int(patch_size/100)))
+        return image
+    
+    else:
+        patch = data_arr.copy()
+        d_min = patch.min()
+        if d_min < -3.4e30:
+            set_data_val(patch, d_min, 0.0)
+
+        patch = np.copy(patch)
+        patch = scale_array(patch, 0, 256)
         patch = patch.astype(np.uint8)
         image = Image.fromarray(patch, mode='L')
         return image.resize((patch_size, patch_size), resample=Image.HAMMING)
@@ -190,6 +238,8 @@ def generate_patches(raw_data, sample_coords, directory, location, patch_size, s
         
         # First generate the data channels from the preprocessed Raw Data
         for channel in raw_data.keys():
+            print('Building %s/%s/%s/%s' % (patch_size, scale, data_type, channel))
+
             data_samples = []
             raw_data_arr = raw_data[channel]
             for sample_coord in sample_coords[data_type]:
@@ -238,11 +288,13 @@ def generate_dataset(directory, location, patch_sizes, num_samples):
         'val':          [(random.randint(y_min, y_max), random.randint(x_min, x_max)) for _ in range(num_samples['val'])]
     }
 
-    print(sample_coords['train'])
+    # print(sample_coords['train'])
 
     for i, patch_size in enumerate(patch_sizes):
         scales = [s for s in range(1, 2 ** i + 1)]
         for scale in scales:
             generate_patches(raw_channel_data, sample_coords, directory, location, patch_size, scale)
 
+#?? ------------------------------------- MAIN----------------------------------------------------
 
+generate_dataset(directory, location, patch_sizes, num_samples)
